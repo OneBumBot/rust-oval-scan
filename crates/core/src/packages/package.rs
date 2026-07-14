8c5089bb2a82c732f61449881d163d1c3810ab0e
Iurii Pianov <onebumbot@gmail.com>
2026-07-14 13:44:38 +0300

    reorganize Package struct
    change arch and desc fields from String to Option<String> due to in alpm
    files this sections may be empty or null

--- crates/core/src/packages/package.rs
+++ crates/core/src/packages/package.rs
@@ -2,7 +2,8 @@
 pub struct Package {
     pub name: String,
     pub version: String,
-    pub arch: String,
+    pub desc: Option<String>,
+    pub arch: Option<String>,
     pub license: String,
     pub build_date: String,
     pub install_date: String,
