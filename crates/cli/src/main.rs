ed5b71bd0bf6f160867eb9a587bbab08c95d9578
Iurii Pianov <onebumbot@gmail.com>
2026-07-14 17:14:43 +0300

    Change main function to test new features
    in future i will move tests from main to tests/ folder

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,10 +1,28 @@
 use std::io;
+use std::path::Path;
 
 use oval_scanner::collectors::host_collector::HostCollector;
+use oval_scanner::collectors::package_collector::PackageCollector;
+use oval_scanner::collectors::pacman_collector::PacmanCollector;
+use oval_scanner::runners::local_runner::LocalRunner;
+use oval_scanner::runners::runner::{self, Runner};
 
 fn main() -> io::Result<()> {
     let host = HostCollector::collect()?;
 
     println!("Host: {host:#?}");
+
+    let runner = LocalRunner {};
+
+    let collector = PacmanCollector::new(runner);
+
+    let name = collector.name();
+    let detect = collector.detect()?;
+
+    if detect {
+        println!("{} was detected", name);
+        collector.collect()?;
+    }
+
     Ok(())
 }
