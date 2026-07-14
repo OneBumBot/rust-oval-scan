6b5ddcd8ecb41c807c9be713605a10768e66b47c
Iurii Pianov <onebumbot@gmail.com>
2026-07-14 22:31:20 +0300

    Remove prints from pacman collector
    Remove unnecessary prints due to trash in stdout

--- crates/scanner/src/collectors/pacman_collector.rs
+++ crates/scanner/src/collectors/pacman_collector.rs
@@ -24,20 +24,18 @@ impl<R: Runner> PacmanCollector<R> {
 
         for line in content.lines() {
             match line {
-                "" => println!(),
+                "" => (),
 
                 val if val.starts_with("%") && val.ends_with("%") => {
                     if let Some(field) = current_field.take() {
                         desc.insert(field, std::mem::take(&mut values));
                     }
                     current_field = Some(val.trim_matches('%').to_owned());
-                    println!("{}", val.trim_matches('%').to_owned());
                 }
 
                 val => {
                     if current_field.is_some() {
                         values.push(val.to_owned());
-                        println!("{}", val)
                     }
                 }
             }
