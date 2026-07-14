794190e421a9fc2448d3cb118e7a7f39dcd8769b
Iurii Pianov <onebumbot@gmail.com>
2026-07-14 17:02:30 +0300

    Create trait for runner

--- crates/scanner/src/runners/runner.rs
+++ crates/scanner/src/runners/runner.rs
@@ -1,8 +1,17 @@
 use std::io::Result;
+use std::path::Path;
 use std::process::Output;
 
-trait Runner {
-    fn execute(&self, command: &str, args: &str) -> Result<Output>;
-    fn is_file_exist(&self, path: &str) -> Result<bool>;
-    fn open_file(&self, path: &str) -> String;
+#[derive(Debug)]
+pub struct CommandOutput {
+    pub exit_code: Option<i32>,
+    pub stdout: String,
+    pub stderr: String,
+}
+
+pub trait Runner {
+    fn execute(&self, command: &str, args: &[&str]) -> Result<CommandOutput>;
+    fn file_exist(&self, path: &Path) -> Result<bool>;
+    fn command_exits(&self, command: &str) -> Result<bool>;
+    fn open_file(&self, path: &Path) -> Result<String>;
 }
