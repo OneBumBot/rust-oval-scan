f84173115eb9e7cf30ecb08b9b85a4cbc47b3382
Iurii Pianov <onebumbot@gmail.com>
2026-08-04 00:35:19 +0300

    Made select_package_collectors
    select_package_collectors(runner: R) get a pointer to any struct that
    implements runner trait, scan host for package managers and return valid
    package managers

--- crates/scanner/src/collectors.rs
+++ crates/scanner/src/collectors.rs
@@ -1,3 +1,31 @@
 pub mod host_collector;
 pub mod package_collector;
 pub mod pacman_collector;
+
+use crate::{
+    collectors::{package_collector::PackageCollector, pacman_collector::PacmanCollector},
+    runners::runner::Runner,
+};
+use futures::io;
+use tracing;
+
+#[tracing::instrument(name = "collectors.select_collector", skip(runner), err)]
+pub async fn select_package_collectors<'a, R: Runner>(
+    runner: &'a R,
+) -> io::Result<Vec<Box<dyn PackageCollector + 'a>>> {
+    let mut res: Vec<Box<dyn PackageCollector + 'a>> = Vec::new();
+
+    let collectors = vec![Box::new(PacmanCollector::new(runner))];
+
+    for collector in collectors {
+        if collector.detect().await? {
+            tracing::info!(collector = collector.name(), "Package collector detected");
+            res.push(collector);
+        }
+    }
+    tracing::trace!(
+        collecotrs.count = res.len(),
+        "Package collectors detecion complete"
+    );
+    Ok(res)
+}
