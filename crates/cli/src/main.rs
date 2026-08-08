46db603493434568ceb550198ab2afd9a08d9027
Iurii Pianov <onebumbot@gmail.com>
2026-08-08 17:55:47 +0300

    Edit main.rs for cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,16 +1,11 @@
 use chrono::{DateTime, Local};
-use oval_scanner::collectors;
+use oval_scanner::runners::local_runner::LocalRunner;
+use oval_scanner::scan::scan_host;
+use std::fs::File;
+use std::io::Write;
 use std::io::{self, IsTerminal};
 use std::time::{Instant, SystemTime};
 use tracing_subscriber::EnvFilter;
-
-use oval_scanner::{
-    collectors::{
-        host_collector::HostCollector, package_collector::PackageCollector,
-        pacman_collector::PacmanCollector,
-    },
-    runners::local_runner::LocalRunner,
-};
 
 fn format_time(time: SystemTime) -> String {
     DateTime::<Local>::from(time)
@@ -32,6 +27,14 @@ fn init_tracing() {
         .init()
 }
 
+fn dump_inventory_to_file(host: &oval_core::hosts::Host) -> io::Result<()> {
+    let mut file = File::create("inventory.json")?;
+    let json = serde_json::to_string_pretty(host)?;
+    file.write_all(json.as_bytes())?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     init_tracing();
@@ -40,47 +43,28 @@ async fn main() -> io::Result<()> {
 
 #[tracing::instrument(name = "scan", skip_all, err)]
 async fn run() -> io::Result<()> {
+    let started_at = SystemTime::now();
+    let timer = Instant::now();
+
     let runner = LocalRunner;
 
-    let host = HostCollector::collect(&runner).await?;
+    let host = scan_host(&runner).await?;
 
-    println!("Host: {host:#?}");
-
-    let collectors = collectors::select_package_collectors(&runner).await?;
-
-    if collectors.is_empty() {
-        tracing::warn!("No supported collectors detected");
-    }
-
-    for collector in collectors {
-        let started_at = SystemTime::now();
-        let timer = Instant::now();
-        let packages = collector.collect().await?;
-        let elapsed = timer.elapsed();
-        let finished_at = SystemTime::now();
-        let packages_to_take = 40;
-        println!("Show first {} packages:", packages_to_take);
-        for package in packages.iter().take(packages_to_take) {
-            println!(
-                "{} {} (installed: {}, built: {})",
-                package.name,
-                package.version,
-                format_time(package.install_date),
-                format_time(package.build_date),
-            );
-        }
-
-        println!(
-            "Collection started: {}\n\
+    let elapsed = timer.elapsed();
+    let finished_at = SystemTime::now();
+    println!("Host: {}", serde_json::to_string(&host)?);
+    println!(
+        "Collection started: {}\n\
             Collection finished: {}\n\
             Collection duration: {:.2?}\n\
             Packages collected: {}",
-            format_time(started_at),
-            format_time(finished_at),
-            elapsed,
-            packages.len(),
-        );
-    }
+        format_time(started_at),
+        format_time(finished_at),
+        elapsed,
+        host.packages.len(),
+    );
+
+    dump_inventory_to_file(&host);
 
     Ok(())
 }
